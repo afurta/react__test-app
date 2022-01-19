3a8e1183a33520d0cb72b16fe5569ff24415299b
afurta <furtaa@mail.ru>
2022-01-19 17:00:38 +0500

    change

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
