7a61ae7f35c455b7cdddd3bd9a2a29f8fb9b331b
afurta <furtaa@mail.ru>
2022-01-19 17:14:09 +0500

    n

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>,
